ed7437061755db1f185170a159c2a7607a8cdc3f
Luiz Guilherme Avelino <guilhermerx18@gmail.com>
2022-03-11 16:52:36 -0300

    requisito 8

--- src/components/EmptyCart.js
+++ src/components/EmptyCart.js
@@ -1,16 +1,20 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class EmptyCart extends Component {
   render() {
     const { listCart } = this.props;
-    const quantity = listCart.map((item) => item.id);
-    const renderItems = listCart.map((item, index) => (
-      <div key={ index }>
-        <p data-testid="shopping-cart-product-name">{item.title}</p>
-        <p data-testid="shopping-cart-product-quantity">{item.price}</p>
+    const allItems = listCart.map((item) => item.id);
 
-      </div>
-    ));
+    const renderItems = listCart.map((item, index) => {
+      const filter = allItems.filter((obj) => obj === item.id);
+      return (
+        <div key={ index }>
+          <p data-testid="shopping-cart-product-name">{item.title}</p>
+          <p data-testid="shopping-cart-product-quantity">{filter.length}</p>
+        </div>
+      );
+    });
 
     return (
       <div>
@@ -22,5 +26,9 @@ class EmptyCart extends Component {
     );
   }
 }
+
+EmptyCart.propTypes = {
+  listCart: PropTypes.array,
+}.isRequired;
 
 export default EmptyCart;
