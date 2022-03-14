878b7f47b4a898a5e4d17099ee00587c6700a4d7
Pedro Fideles <pedro.fideles162@gmail.vom>
2022-03-14 16:54:28 -0300

    Req. 10 - subindo para testes

--- src/components/EmptyCart.js
+++ src/components/EmptyCart.js
@@ -28,31 +28,33 @@ class EmptyCart extends Component {
 
   render() {
     const { listCart, addCart } = this.props;
-    console.log(addCart);
     const cart = this.assembleCart(listCart);
 
-    const renderItems = cart.map((item, index) => (
-      <div key={ index } className={ item.id }>
-        <p data-testid="shopping-cart-product-name">{item.title}</p>
-        <button
-          type="button"
-          onClick={ (event) => addCart(event, true) }
-          className={ item.id }
-          data-testid="product-decrease-quantity"
-        >
-          -
-        </button>
-        <span data-testid="shopping-cart-product-quantity">{item.amount}</span>
-        <button
-          type="button"
-          onClick={ addCart }
-          className={ item.id }
-          data-testid="product-increase-quantity"
-        >
-          +
-        </button>
-      </div>
-    ));
+    const renderItems = cart.map((item, index) => {
+      console.log(item.title);
+      return (
+        <div key={ index } className={ item.id }>
+          <p data-testid="shopping-cart-product-name">{item.title}</p>
+          <button
+            type="button"
+            onClick={ (event) => addCart(event, true) }
+            className={ item.id }
+            data-testid="product-decrease-quantity"
+          >
+            -
+          </button>
+          <span data-testid="shopping-cart-product-quantity">{item.amount.toString()}</span>
+          <button
+            type="button"
+            onClick={ addCart }
+            className={ item.id }
+            data-testid="product-increase-quantity"
+          >
+            +
+          </button>
+        </div>
+      );
+    });
 
     return (
       <div>
